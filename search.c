b4e25950ce90ed0234738abfd97d917ace99250a
SadPlum <92168542+SadPlum@users.noreply.github.com>
2023-01-22 17:44:58 -0400

    need to update expand function

--- search.c
+++ search.c
@@ -1,68 +1,109 @@
-#include <string.h>
+#include <stdio.h>
 #include <stdbool.h>
 
-int search(int array[10][10], int startx,int starty){
+// declerations
+int search(int[10][10], int, int);
+bool outOfBounds(int, int);
+int makeSearchArr(int[10][10], int, int, bool);
+int expand(int[10][10], int[4][2], bool);
 
-    printf("the strarting point, %d, axis' are\nx=%d & y=%d\n", array[starty][startx], startx, starty);
-    int counter =0;
-    int grids[]={}; 
 
+int search(int grid[10][10], int starty, int startx){
+    printf("the strarting point, %d, axis' are\nx=%d & y=%d\n", grid[starty][startx], startx, starty);
     return 0;
 };
 
 // Checks if the x and y coordinates are within the 10x10 grid
 bool outOfBounds(int x, int y) {
-    if(x > 0 && x < 10) {
-        if(y > 0 && y < 10) {
-            return true;
+    if(x >= 0 && x < 10) {
+        if(y >= 0 && y < 10) {
+            return false;
         }
     }
-    return false;
+    return true;
 };
 
-int firstArr(int grid[10][10], int startx,int starty) {
-    int indexesx[2] = {startx+1, startx-1};
-    int indexesy[2] = {starty+1, starty-1};
-    int array[]={};
+int makeSearchArr(int grid[10][10], int starty,int startx, bool found) {
+      if(found == true) { 
+        return 0;
+    };     
+     // adds count to loop over elements in array
     int indexCounter =0;
-    for( int i=0; i< 2; i++) {
-        for(int j=0; j< 2; j++) {
-        
-            int x = indexesx[i];
-            int y = indexesy[j];
-            if(outOfBounds(x,y) == true){
-                printf("this is x %d, this is y %d\n",x,y);
-                if(grid[x][y]== 0) {
-                    printf("this is grid %d\n",grid[x][y]);
-                    int arr[2] = {i,j};
-                    array[indexCounter] == arr;
+    // adds all the vertical and horizontal coordinates from the starting point. 
+    // given a start of 9,5, the indexes array will contain {{5,8}, {4,9}, {6,9}, {5,10}}
+    int indexes[4][2] = {{startx, starty-1}, {startx-1, starty}, {startx+1, starty}, {startx, starty+1}};
+    int array[4][2];
+    for( int i=0; i< 4; i++) {
+            int x = indexes[i][0];
+            int y = indexes[i][1];
+            // checks if the coordinates are within grid
+            if(outOfBounds(y,x) == false){
+                // checks if the coordinates a passable value printf("%d", grid[5][9]);
+                if(grid[y][x]==4) {
+                    printf("found");
+                    found = true;
+                    return grid;
+                }
+                if(grid[y][x]== 0) {
+                    array[indexCounter][0] = x;
+                    array[indexCounter][1] = y;
                     indexCounter++;
                 }
-            }
         }
-    }   
-
-    printf("array is %d", array);
-    return array;
+    } 
+    if(found == false) {
+    expand(grid, array, found);
+    }
 };
+
+
+int expand(int grid[10][10], int coordinatesArray[4][2], bool found ) {
+    if(found == true) { 
+        return grid;
+    };
+    for(int i = 0; i < 4; i++) {
+        int x = coordinatesArray[i][0];
+        int y = coordinatesArray[i][1];
+        if (outOfBounds(y, x) == false){  
+                if(found == false) {
+                    if(grid[y][x] == 0) {
+                    printf("grid[%d][%d]\n", y,x);
+                    grid[y][x] = 2;
+                    } 
+                }
+        }
+    }
+}
 
 
 int main() {
 // 10 x 10 grid. 1 denotes start. 2 denotes end.
 
     int grid[10][10] = {
-        {0,0,0,0,0,2,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,4,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,1,0,0,0,0},
         } ;
-    search(grid, 5,9);
-    firstArr(grid,5,9);
+    bool found = false;
+     printf("%d", grid[6][5]);
+    search(grid, 9,5);
+    makeSearchArr(grid, 9,5, found);
+    
+          printf("\ngrid:");
+                    for(int i=0; i<10; i++) {
+                        printf("\n");
+                        for(int j=0; j<10; j++) { 
+                            printf("%d", grid[i][j]);
+                        }
+                    }
+
     return 0;
+
 };
